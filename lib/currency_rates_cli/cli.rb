a7de128fd97ce1962f364c2c4ef516f55db3c819
lizaetillman <lizaetillman@gmail.com>
2020-08-08 22:13:31 -0400

    cli project

--- lib/currency_rates_cli/cli.rb
+++ lib/currency_rates_cli/cli.rb
@@ -2,6 +2,7 @@ class CLI
 
     def run
         API.get_data
+        puts " "
         puts "Welcome to the Currency Rates CLI!"
         main_menu
     end
@@ -9,11 +10,13 @@ class CLI
     def main_menu
         input = " "
         while input != "exit"
-            puts "This program will reveal the current currency rate at the selected country."
+            puts " "
+            puts "This program will reveal the current currency rate for the country you choose!"
             puts "All currency conversion rates are based on 1 U.S. Dollar."
             puts " "
             puts "To get the conversion rate, type 'enter'."
             puts "To quit this program, type 'exit'."
+            puts " "
             
             input = gets.strip.downcase
     
@@ -31,14 +34,20 @@ class CLI
     end
     
     def enter
-        puts "Select a country's currency to see its conversion rate."
+        puts " "
+        puts "To see a country's currency rate, please type in the country's corresponding number."
+        puts "(i.e. Type '8' to see Canada's (CAD) currency rate.)"
         puts " "
 
         Currency_Rates.all.each.with_index(1) do |conversion_rate, index|   
             puts "#{index}. #{conversion_rate.name}" 
         end
 
-        puts "Please enter the number of the of currency to exchange the rate."
+        puts " "
+        puts "To see a country's currency rate, please type in the country's corresponding number."
+        puts "(i.e. Type '8' to see Canada's (CAD) currency rate.)"
+        puts " "
+
         input = gets.strip.to_i
 
         #seperate this method
@@ -47,6 +56,7 @@ class CLI
             puts "#{conversion_rate.name} = #{conversion_rate.rate}"
             exit?
         else
+            puts " "
             puts "Invaild! Please choose a valid number." 
             enter
         end
@@ -54,16 +64,20 @@ class CLI
     end
     
     def exit?
+        puts " "
         puts "Would you like to exit or continue? Type 'exit' or 'continue'."
+        puts " "
         input = gets.chomp.downcase
 
         if input == "exit"
+            puts " "
             puts "Thank you for using the Currency Rates CLI! Goodbye!"
             exit
         elsif input == "continue"
             enter
         else
-            puts "Invaild! Please choose a valid currency code."
+            puts " "
+            puts "Invaild! Let's try again."
             exit?
         end
 
